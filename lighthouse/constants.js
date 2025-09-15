5c2870051b02ffcb066a7c91bca43281b21d5f66
Vladomira <juliavladomira@gmail.com>
2025-09-15 15:06:38 +0200

    fix(#15): fix failing lighthouse test due to config

--- lighthouse/constants.js
+++ lighthouse/constants.js
@@ -1,9 +1,9 @@
-import { resolve } from 'path';
+const { resolve } = require('path');
 
 require('dotenv').config({ path: resolve(__dirname, '../.env') });
 
 const baseUrl = process.env.REACT_APP_PROD_HOST_API_URL || 'http://localhost:3001';
 
-export default {
+module.exports = {
   baseUrl,
 };
