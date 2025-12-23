ee3bc92f05696223b77aa2d0ccaa5eca1d29b948
RudoiDmytro <irudoj63@gmail.com>
2025-12-23 10:56:08 +0200

    Expose schema fetch helpers via lib index

---
+++ docker/apollo-server/lib/index.ts
@@ -0,0 +1,4 @@
+export { fetchAndSaveSchema, getLogger, handleFinalError, handleFatalError } from './schemaFetcher';
+export { startServer, gracefulShutdownAndExit } from './server';
+export { resolvers } from './resolvers';
+export { formatError } from './formatError';
